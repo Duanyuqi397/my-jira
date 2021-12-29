d3f5a17106f281608ccde5b6a9eebe86aa735562
yuqi.duan <yuqi.duan@weimob.com>
2021-12-29 15:49:29 +0800

    登录注册页样式优化

--- src/unauthenticated-app/login.tsx
+++ src/unauthenticated-app/login.tsx
@@ -2,6 +2,7 @@ import { login } from "auth-provider";
 import { useAuth } from "context/auth-context";
 import React, { FormEvent } from "react";
 import { Form, Input, Button } from "antd";
+import { LongButton } from "unauthenticated-app";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
@@ -27,9 +28,9 @@ export const LoginSreen = () => {
         <Input placeholder="密码" type="password" id={"password"} />
       </Form.Item>
       <Form.Item>
-        <Button htmlType="submit" type={"primary"}>
+        <LongButton htmlType="submit" type={"primary"}>
           登录
-        </Button>
+        </LongButton>
       </Form.Item>
     </Form>
   );
--- src/unauthenticated-app/register.tsx
+++ src/unauthenticated-app/register.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from "context/auth-context";
 import React, { FormEvent } from "react";
 import { Button, Form, Input } from "antd";
+import { LongButton } from "unauthenticated-app";
 
 export const RegisterScreen = () => {
   const { register, user } = useAuth();
@@ -25,10 +26,11 @@ export const RegisterScreen = () => {
         <Input placeholder="密码" type="password" id={"password"} />
       </Form.Item>
       <Form.Item>
-        <Button htmlType="submit" type={"primary"}>
+        <LongButton htmlType="submit" type={"primary"}>
           注册
-        </Button>
+        </LongButton>
       </Form.Item>
     </Form>
   );
 };
+LongButton;
